7b0718c9ef6da7b36d0345782b4fd2a06aa12a51
Yossef Hamdy <yousef.hamdy@wexcute.com>
2023-12-05 14:34:26 +0200

    test

---
+++ index.js
@@ -0,0 +1,10 @@
+function eventTest() {
+  const setBg = () => {
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+  };
+
+  circle.addEventListener("click", setBg);
+  setBg();
+}
